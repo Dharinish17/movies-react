5c00732ecd7ca8b25a863d260a3d410a0651b554
Dharinish17 <dharinish2006@gmail.com>
2026-06-17 20:39:21 +0530

    add search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,23 +3,25 @@ import ak from "../api.txt?raw";
 import { useNavigate } from "react-router-dom";
 
 function Search({ type = "Movies" }) {
-  const [query, setQuery] = useState("");
-  const [data, setData]= useState([]);
-  const searchRef= useRef();
-  const navigate= useNavigate();
+  // const [query, setQuery] = useState("");
+  // const [data, setData] = useState([]);
+  const searchRef = useRef();
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-     if (!query.trim()) return;
+  // useEffect(() => {
+  //   if (!query.trim()) return;
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${ak}&query=${query}`)
-    .then((response)=> response.json())
-    .then((d)=> {
-      setData(d.results  || []);
-    })
-    .catch((err)=> {
-      console.log(err);
-    });
-  }, [query])
+  //   fetch(
+  //     `https://api.themoviedb.org/3/search/movie?api_key=${ak}&query=${query}`,
+  //   )
+  //     .then((response) => response.json())
+  //     .then((d) => {
+  //       setData(d.results || []);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, [query]);
 
   return (
     <div className="flex w-full justify-center p-2">
@@ -28,23 +30,24 @@ function Search({ type = "Movies" }) {
         className="bg-white w-10/12 md:w-11/12 text-gray-800 p-1 rounded-l-sm focus:outline-0 md:bg-amber-200 lg:bg-red-600"
         placeholder={`Search ${type}`}
         onKeyDown={(e) => {
-          if(e.key=== "Enter"){
-          setQuery(e.target.value)
-          navigate(`/search/${searchRef.current.value}`)
-          searchRef.current.value=""
-        }}}
-        ref= {searchRef}
+          if (e.key === "Enter" && searchRef.current.value.trim() !="") {
+            // setQuery(e.target.value);
+            navigate(`/search/${searchRef.current.value}`);
+            searchRef.current.value = "";
+          }
+        }}
+        ref={searchRef}
       />
       <input
         type="button"
         value="Search"
         className="bg-gray-800 p-1 rounded-r-sm hover:bg-gray-900 cursor-pointer"
-        onClick={(e)=> {
-            setQuery(searchRef.current.value)
-            navigate(`/search/${searchRef.current.value}`)
-            searchRef.current.value= ""
-          }
-          }
+        onClick={(e) => {
+          // setQuery(searchRef.current.value);
+          if(searchRef.current.value.trim() === ""){return}
+          navigate(`/search/${searchRef.current.value}`);
+          searchRef.current.value = "";
+        }}
       />
     </div>
   );
--- src/components/SearchRes.jsx
+++ src/components/SearchRes.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { data, useParams } from "react-router-dom";
+import ak from "../api.txt?raw";
+import Card from "./Card";
 
 function SearchRes() {
-  const {movieName}= useParams();
+  const { movieName } = useParams();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (movieName.trim() === "") {
+      return;
+    }
+
+    const movie = movieName.trim();
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${ak}&query=${encodeURIComponent(movie)}`,
+    )
+      .then((Response) => Response.json())
+      .then((data) => {
+        setData(data.results || []);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [movieName]);
+
   return (
-    <div className='text-white font-bold text-4xl'>{`searchRes: ${movieName}`}</div>
-  )
+    <div className="mb-4 p-4">
+      <h2 className="text-white text-3xl font-bold mb-2">{`Search for ${movieName}:`}</h2>
+
+      {data.length > 0 ? (
+        <div className="overflow-x-auto mt-2 scrollbar scrollbar-thumb-gray-500 scrollbar-thin scrollbar-track-transparent">
+          <div className="flex w-max gap-4 p-1">
+            {data.map((movie) => (
+              <Card
+                name={movie.title}
+                date={movie.release_date}
+                link={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+                movieid={movie.id}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="text-red-500 text-2xl">{`Oops! We couldn't find any movies matching "${movieName}".`}</div>
+      )}
+    </div>
+  );
 }
 
-export default SearchRes
+export default SearchRes;
