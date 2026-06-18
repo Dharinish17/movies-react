82a0cd6566ff03721c2be4ecfb945923d37524e3
Dharinish17 <dharinish2006@gmail.com>
2026-06-19 01:08:07 +0530

    fix no similar movies

--- src/components/Similar.jsx
+++ src/components/Similar.jsx
@@ -16,9 +16,9 @@ function Similar({movieId, movieName}) {
         <p className='text-white font-bold text-3xl'>Similar to {movieName}</p>
         <div className='overflow-x-auto mt-2 scrollbar scrollbar-thumb-gray-500 scrollbar-thin scrollbar-track-transparent'>
             <div className='flex w-max gap-4 p-1'>
-                {data.map((movie)=> (
+                {data.length>0 ? (data.map((movie)=> (
                     <Card name={movie.title} date={movie.release_date} link={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} movieid={movie.id} />
-                ))}
+                ))) : <p className='text-gray-400 text-lg md:text-2xl'>No Similar movies found</p>}
             </div>
         </div>
     </div>
